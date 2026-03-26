7777700499be6a87dcbcaf1a0efc657d2b347a16
Atul <dr6217811@gmail.com>
2026-03-26 21:32:27 +0530

    handlers not working

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import express from "express";
 import { PORT } from "./configs/serverConfig.js";
 import { connectDB } from "./configs/dbConfig.js";
-import v1Router from "./routes/index.js";
 
 
 const app = express();
 
-
-app.use('/api', v1Router);
 
 app.listen(PORT, async () => {
     try {
